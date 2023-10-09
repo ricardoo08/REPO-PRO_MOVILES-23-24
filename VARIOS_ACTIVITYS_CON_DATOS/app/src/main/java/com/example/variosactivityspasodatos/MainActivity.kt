c89983d0c2688be783d29c68a11fcc3057358249
Ricar <gmezricar@gmail.com>
2023-10-09 18:07:09 +0200

    COMMIT DE TODO EL EJEMPLO

--- VARIOS_ACTIVITYS_CON_DATOS/app/src/main/java/com/example/variosactivityspasodatos/MainActivity.kt
+++ VARIOS_ACTIVITYS_CON_DATOS/app/src/main/java/com/example/variosactivityspasodatos/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.variosactivityspasodatos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.variosactivityspasodatos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.boton.setOnClickListener {
+            irAVentana2()
+        }
+    }
+
+    private fun irAVentana2() {
+        //podriamos crear aqui el objeto y pasarlo,en vez de los atributos individualmente.
+        // Persona deberia ser serializable en ese caso
+        var miIntent: Intent = Intent(this, Ventana_2::class.java)
+        //miIntent.putExtra("nombre",binding.cajaNombre.text.toString())
+        //miIntent.putExtra("edad",binding.cajaEdad.text.toString())
+        startActivity(miIntent)
     }
 }
---
+++ VARIOS_ACTIVITYS_CON_DATOS/app/src/main/java/com/example/variosactivityspasodatos/Ventana_2.kt
@@ -0,0 +1,11 @@
+package com.example.variosactivityspasodatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ventana_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ventana2)
+    }
+}
