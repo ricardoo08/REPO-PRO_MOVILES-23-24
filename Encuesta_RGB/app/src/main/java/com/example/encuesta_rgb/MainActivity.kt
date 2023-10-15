fe114b20da3dac25c2bc1eb9524f3f75a025fce9
Ricar <gmezricar@gmail.com>
2023-10-15 19:00:39 +0200

    Xml hecho

--- Encuesta_RGB/app/src/main/java/com/example/encuesta_rgb/MainActivity.kt
+++ Encuesta_RGB/app/src/main/java/com/example/encuesta_rgb/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.encuesta_rgb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.SeekBar
+import android.widget.Toast
 import com.example.encuesta_rgb.databinding.ActivityMainBinding
 
 lateinit var binding: ActivityMainBinding
@@ -9,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     lateinit var nombre :String
     lateinit var sisOp : String
     lateinit var espe : String
+    var horasEst : Int = 0
+    val encuestasList = ArrayList<Encuesta>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
@@ -16,9 +21,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         if (binding.swAnonimo.isChecked) {
             nombre = "Anónimo"
+            binding.txtNom.setText("Anónimo")
         } else {
-            nombre = binding.cajaNombres.text.toString()
+            nombre = binding.txtNom.text.toString()
         }
+
         binding.gruSis.setOnCheckedChangeListener { group, checkedId ->
             when (checkedId) {
                 binding.cirLin.id -> {
@@ -49,5 +56,44 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+        horasEst= binding.seekBar.progress
+        binding.txtBar.setText(horasEst.toString())
+    }
+    fun Validar(view: View) {
+        val encuesta = Encuesta(nombre, sisOp, espe, horasEst)
+        encuestasList.add(encuesta)
+        nombre = "Anónimo"
+        sisOp = ""
+        espe = ""
+        horasEst = 0
+        binding.cajaNombres.text = ""
+    }
+    fun Cuantas(view: View) {
+        val totalEncuestas = encuestasList.size
+        val mensaje = "Total de encuestas realizadas: $totalEncuestas"
+        Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show()
+    }
+    fun Resumen(view: View) {
+        val resumenEncuestas = StringBuilder()
+
+        for (encuesta in encuestasList) {
+            resumenEncuestas.append(encuesta.toString())
+            resumenEncuestas.append("\n\n")
+        }
+        binding.cajaNombres.text = resumenEncuestas.toString()
+    }
+    fun Reiniciar(view: View) {
+        encuestasList.clear()
+        nombre = ""
+        sisOp = ""
+        espe = ""
+        horasEst = 0
+        binding.cajaNombres.text = ""
+        binding.txtBar.text = ""
+        binding.txtNom.text.clear()
+        binding.swAnonimo.isChecked = false
+        binding.seekBar.progress = 0
+        binding.gruSis.clearCheck()
+        binding.gruEsp.clearCheck()
     }
 }
