e674306c0d56feffe702d1276888635255e3fac0
Ricar <gmezricar@gmail.com>
2023-10-19 18:36:20 +0200

    Interfaz hecha

--- TresEnRaya_RGB/app/build.gradle.kts
+++ TresEnRaya_RGB/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.tresenraya_rgb"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.tresenraya_rgb"
